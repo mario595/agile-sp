5272300e2030524b83f3678d2e899d5f79817ec1
mario595 <mariomartinez595@gmail.com>
2014-12-18 17:17:49 +0000

    Added feature for change name

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -12,6 +12,10 @@ function AppCtrl($scope, socket) {
     $scope.users.push(data.name);
   });
 
+  socket.on('change:name', function (data) {
+    changeName(data.oldName, data.newName);
+  });
+
   // add a message to the conversation when a user disconnects or leaves the room
   socket.on('user:left', function (data) {
     var i, user;
@@ -23,4 +27,38 @@ function AppCtrl($scope, socket) {
       }
     }
   });
+
+  // Private helpers
+  // ===============
+
+  var changeName = function (oldName, newName) {
+    // rename user in list of users
+    var i;
+    for (i = 0; i < $scope.users.length; i++) {
+      if ($scope.users[i] === oldName) {
+        $scope.users[i] = newName;
+      }
+    }
+  }
+
+// Methods published to the scope
+  // ==============================
+
+  $scope.changeName = function () {
+    socket.emit('change:name', {
+      name: $scope.newName
+    }, function (result) {
+      if (!result) {
+        alert('There was an error changing your name');
+      } else {
+
+        changeName($scope.name, $scope.newName);
+
+        $scope.name = $scope.newName;
+        $scope.newName = '';
+      }
+    });
+  };
+
+
 }
